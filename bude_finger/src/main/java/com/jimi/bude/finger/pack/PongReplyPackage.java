fc31245e09a0df5325915607f4cd60b74fafcce2
trjie <943614362@qq.com>
2018-09-29 11:13:28 +0800

    Server1.0.0版本

--- bude_finger/src/main/java/com/jimi/bude/finger/pack/PongReplyPackage.java
+++
@@ -1,9 +0,0 @@
-package com.jimi.bude.finger.pack;
-
-import cc.darhao.jiminal.annotation.Protocol;
-import cc.darhao.jiminal.pack.BasePackage;
-
-@Protocol(0x51)
-public class PongReplyPackage extends BasePackage{
-
-}
